770e0e593c80d7a724cfd1bc77ba1025d7843419
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-12-07 20:06:59 +0330

    add a part of logic of next button in question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React , {useState} from 'react'
+import Card from '../layout/Card'
+import InputQuestion from './INPUT/InputQuestion';
+
+const qustionDatas = [
+  {
+    id: Math.random(),
+    type: "email",
+    questionContent: "لطفا ایمیل خود را وارد کنید:",
+    corectAnswer: 19
+  } ,
+  {
+    id: Math.random(),
+    type: "number",
+    questionContent: "لطفا شماره تماس خود را وارد کنید:",
+    corectAnswer: 19
+  },
+  {
+    id: Math.random(),
+    type: "number",
+    questionContent:"whats your phone number",
+    corectAnswer: 19
+  }
+]
+
 
 function Question() {
+  const [content , setContent] = useState(qustionDatas[0].questionContent)
+  
+  let i = 0;
+  const nextQuestionHandler = () => {
+    i ++;
+    let item = qustionDatas[i]
+    if(i === 0){
+      console.log("mamad")
+    }
+    setContent(item.questionContent);
+    console.log(item.questionContent)
+    console.log(i)
+  }
   return (
-    <div></div>
+    <Card>
+      <InputQuestion description={content} label={"Email"} type={"email"}/>
+      <button onClick={nextQuestionHandler}>next</button>
+    </Card>
   )
 }
 
