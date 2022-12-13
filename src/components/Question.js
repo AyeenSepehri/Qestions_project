0635f96ef792a620e24b742135ee3a4e9dea7a2c
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-12-13 19:16:00 +0330

    work on counter of question

--- src/components/Question.js
+++ src/components/Question.js
@@ -9,11 +9,13 @@ const questionDatas = [
     id: Math.random(),
     type: "INPUT",
     questionContent: "لطفا ایمیل خود را وارد کنید:",
+    userAnswer: "",
   } ,
   {
     id: Math.random(),
     type: "INPUT",
     questionContent: "لطفا شماره تماس خود را وارد کنید:",
+    userAnswer: "",
   },
   {
     id: Math.random(),
@@ -30,52 +32,42 @@ const questionDatas = [
 
 
 function Question() {
-  //state of question content
-  const [content , setContent] = useState()
-  //state of enabel and disabel next button
-  const [status , setStatus] = useState(true)
-  
   //initial counter
-  let i = 0;
-  //onClick next button
-  const nextQuestionHandler = (event) => {
-    event.preventDefault()
-    console.log(questionDatas)
-    const obj = questionDatas[i]
-    console.log(i)
+  let [i , setI] = useState(0)
+  //state of question content
+  const [content , setContent] = useState(questionDatas[0].questionContent)
+
+
+  //function for define content of question
+  const contentIdentifire = (counter) => {
+    console.log(counter)
+
+    const obj = questionDatas[counter]
     console.log(obj)
+
     const text = obj.questionContent
     console.log(text)
 
-    //buuuuuuuuug
+    //set question
     setContent(text)
+  }
 
+  //onClick next button
+  const nextQuestionHandler = () => {
+    
+    //call caption of question function
+    contentIdentifire(i)
+    
+    //set counter
+    setI(i++)
 
-    i++
+    //returning counter to 0
     if(i > questionDatas.length - 1){
-      i = 0
+      setI(0)
     }
+    console.log(i)
     
   }
-console.log(i)
-  // let item = questionDatas[i]
-  // //onClick preview button
-  // const prevBtnHandler = () => {
-
-  //   i --;
-  //   if(i < 0 ){
-  //     i = 0
-  //   }
-  //   setContent(item.questionContent);
-  //   console.log(item.questionContent)
-  //   console.log(i)
-  // }
-
-  // const changeInputHandler = (content) => {
-  //   setStatus(content)
-  // }
-
-  // console.log(item.type)
   return (
     <Card>
       {/* {item.type === "INPUT" ? <InputQuestion description={content} label={"Email"} type={"email"} onChangeInput={changeInputHandler}/> : console.log("hello ayeen")} */}
