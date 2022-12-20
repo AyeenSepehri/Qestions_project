5380eab44a5e64106003451779ac0b9a76970568
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-12-20 19:13:02 +0330

    use redux for share state in question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,85 +1,30 @@
-import React , {useState} from 'react'
+import React from 'react'
 import Card from '../layout/Card'
 import InputQuestion from './INPUT/InputQuestion';
-
-
-//datas of questions section 
-const questionDatas = [
-  {
-    id: Math.random(),
-    type: "INPUT",
-    questionContent: "لطفا ایمیل خود را وارد کنید:",
-    userAnswer: "",
-  } ,
-  {
-    id: Math.random(),
-    type: "INPUT",
-    questionContent: "لطفا شماره تماس خود را وارد کنید:",
-    userAnswer: "",
-  },
-  {
-    id: Math.random(),
-    type: "DROP_DOWN",
-    questionContent:"لطفا تاریخ تولد خود را وارد کنید",
-  },
-  {
-    id: Math.random(),
-    type: "RADIO",
-    questionContent: "لطفا جنسیت خود را تعیین کنید",
-    answers: ["مرد"  , "زن" , "سایر"],
-  }
-]
+import { useSelector , useDispatch } from 'react-redux';
+import { questionInput } from '../store/store';
+import RadioQuestion from './Radio/RadioQuestion';
+import DropDownQuestion from './Drop_Down/DropDownQuestion';
 
 
 function Question() {
-  //initial counter
-  let [i , setI] = useState(0)
-  //state of question content
-  const [content , setContent] = useState(questionDatas[0].questionContent)
-
-
-  //function for define content of question
-  const contentIdentifire = (counter) => {
-    console.log(counter)
-
-    const obj = questionDatas[counter]
-    console.log(obj)
-
-    const text = obj.questionContent
-    console.log(text)
-
-    //set question
-    setContent(text)
-  }
+  //
+  let questionType = useSelector((state) => state.inputType.questionType)
+  const dispatch = useDispatch()
 
   //onClick next button
   const nextQuestionHandler = () => {
-    
-    //call caption of question function
-    contentIdentifire(i)
-    
-    //set counter
-    setI(i++)
-
-    //returning counter to 0
-    if(i > questionDatas.length - 1){
-      setI(0)
-    }
-    console.log(i)
-    
+    dispatch(questionInput.question())
+    dispatch(questionInput.type())
+    dispatch(questionInput.counter())
+    console.log(questionType)
   }
   return (
     <Card>
-      {/* {item.type === "INPUT" ? <InputQuestion description={content} label={"Email"} type={"email"} onChangeInput={changeInputHandler}/> : console.log("hello ayeen")} */}
-      {/* <label>{content}</label> */}
-      {/* <input onChange={changeInputHandler}/> */}
-      <p>{content}</p>
+      {questionType === "INPUT" && <InputQuestion/>}
+      {questionType === "RADIO" && <RadioQuestion/>}
+      {questionType === "DROP_DOWN" && <DropDownQuestion/> }
       <button onClick={nextQuestionHandler}>next</button>
-      {/* <button onClick={prevBtnHandler}>preview</button> */}
-
-
-      {/* {status && <button onClick={prevBtnHandler} disabled>preview</button>}
-      {!status && <button onClick={prevBtnHandler}>preview</button>} */}
     </Card>
   )
 }
