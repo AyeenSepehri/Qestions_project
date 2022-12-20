abb3446b76cc10f81cb41e8284e93ce5a8b61bf2
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-12-20 19:17:13 +0330

    create radio component with mui

---
+++ src/components/Radio/RadioQuestion.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import { useSelector } from 'react-redux';
+
+function RadioQuestion() {
+  let questionContent = useSelector((state) => state.inputType.questionContent);
+  
+    return (
+        <div>
+            <h2>{questionContent}</h2>
+            <FormControl>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    <FormControlLabel value="other" control={<Radio />} label="Other" />
+                </RadioGroup>
+            </FormControl></div>
+    )
+}
+
+export default RadioQuestion
